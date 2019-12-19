4b72cbd11a1d829ff22f5758e71e24d96eff8ba4
Z Porobic <zporobic@expediagroup.com>
2019-12-19 13:39:30 -0800

    adding a label to the config map

--- grafana-dashboard.tf
+++ grafana-dashboard.tf
@@ -1,9 +1,13 @@
-resource "kubernetes_config_map" "grafanadashboard" {
+resource "kubernetes_config_map" "grafana-dashboard" {
  metadata {
   name = "z-dashboard"
  }
 
  data = {
   config = "${path.module}/templates/grafana-dashboard.json"
+ }
+
+ labels = {
+  grafana_dashboard=true
  }
 }
