e38f388a2e3ef14424c8e9a038503d50a7f63b25
Z Porobic <zporobic@expediagroup.com>
2019-12-19 13:25:48 -0800

    fixed typo

--- grafana-dashboard.tf
+++ grafana-dashboard.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_config_map" "kibana_dashboard" {
  metadata {
-   name = "Zs config map"
+   name = "Zs-grafana-dashboard"
  }
   data = {
    config = "${path.module}/templates/grafana-dashboard.json"
