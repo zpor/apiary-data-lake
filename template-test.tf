5e2900e1afe49e625aecf1fa064e143fd1fe1301
Z Porobic <zporobic@expediagroup.com>
2020-01-14 12:44:19 -0800

    testing dashboard json creation with output

---
+++ template-test.tf
@@ -0,0 +1,40 @@
+//add ocunt to not create paid metrics when not desired
+
+resource "random_integer" "graph_id" {
+  min     = 1
+  max     = 50000
+}
+
+variable "apiary_data_buckets" {
+  type = "list"
+  default = [
+    { bucket1 = "egsp-kafkaconnect" },
+    { bucket2 = "lz-egsp" },
+    { bucket3 = "s3-inventory" },
+  ]
+}
+
+data "template_file" "graphs" {
+  template = "${file("${path.module}/templates/graph.tpl")}"
+  count = "${length(var.apiary_data_buckets)}"
+  vars = {
+    bucket_name = "${element(values(var.apiary_data_buckets[count.index]), 0)}"
+    graph_id = "${range(0, length(var.apiary_data_buckets) * 2, 2)[count.index]}"
+  }
+}
+
+data "template_file" "dashboard" {
+  template = "${file("${path.module}/templates/dashboards.tpl")}"
+  vars = {
+    panels = "[${join(",", data.template_file.graphs.*.rendered)}]"
+  }
+}
+
+//resource "local_file" "print_dashboard" {
+//  content = data.template_file.dashboard.rendered
+//  filename = "/Users/zporobic/eg/zpor/apiary-data-lake/template-test/dashboard-rendered.tpl"
+//}
+
+output "grafana_graph" {
+  value = data.template_file.dashboard.rendered
+}
